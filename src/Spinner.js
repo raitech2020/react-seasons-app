9817a93ffd2f3d855791dcafda13cf65979439ea
raitech2020 <raitech2020@gmail.com>
2021-10-14 04:11:56 +0500

    initial commit

---
+++ src/Spinner.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Spinner = (props) => {
+    return (
+        <div>
+            <div className="ui active dimmer">
+                <div className="ui big text loader">
+                    {props.message}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+Spinner.defaultProps = {
+    message: 'Loading ...'
+}
+
+export default Spinner
